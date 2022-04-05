38ab781c194f8c053e48fcf176c378efb80c690e
David Cassell <david.cassell.18@cnu.edu>
2022-04-05 09:32:19 -0400

    html for lung needs updated

--- StagingPythonFiles/LungStaging.py
+++ StagingPythonFiles/LungStaging.py
@@ -1,2 +1,36 @@
+stagingDictionary = {}
+
+path = r'stagingTextFiles\lungStaging.txt'
+
+
 def getValues(requestDict):
-    return None
+    classification = requestDict.get("type")
+    tValue = requestDict.get("T-Value")
+    nValue = requestDict.get('Lymph')
+    if classification == "c":
+        mets = requestDict.get("Clin-Metas")
+    else:
+        mets = requestDict.get("Path-Metas")
+    final_stage = stage(tValue, nValue, mets)
+
+    return final_stage
+
+
+def read_in(fileToRead):
+    file = open(fileToRead, encoding="utf-8")
+
+    for line in file:
+        dictStage = line.split(":")
+        stagingDictionary.update({dictStage[0]: dictStage[1]})
+
+    #print("it was read")
+    file.close()
+
+
+def stage(tValue, nValue, mets):
+    read_in(path)
+    to_calculate = tValue + nValue + mets
+    istage = stagingDictionary.get(to_calculate, "0")
+    #print(istage)
+    stagingDictionary.clear()
+    return istage
